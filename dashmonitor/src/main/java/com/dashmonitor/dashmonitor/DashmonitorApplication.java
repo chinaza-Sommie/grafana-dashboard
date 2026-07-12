aacf7836d6dc37641733a0e44d2c2ab88f6176e8
chinaza-Sommie <chinazachukwunweike35@gmail.com>
2026-07-12 16:05:27 +0100

    first commit

---
+++ dashmonitor/src/main/java/com/dashmonitor/dashmonitor/DashmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.dashmonitor.dashmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashmonitorApplication.class, args);
+	}
+
+}
---
+++ dashmonitor/src/test/java/com/dashmonitor/dashmonitor/DashmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashmonitor.dashmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
